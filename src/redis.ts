c5a7e5f26e1f060c4e500112ae95b9bf370b61b1
Tom Délié <tomdelie78@gmail.com>
2021-06-30 20:34:40 +0200

    chore: add password to redis in production

--- src/redis.ts
+++ src/redis.ts
@@ -2,4 +2,8 @@ import Redis from 'ioredis';
 
 export const redis = new Redis({
     host: process.env.REDIS_ENDPOINT || 'redis_container',
+    port: 6379,
+    ...(process.env.REDIS_PASSWORD
+        ? { password: process.env.REDIS_PASSWORD }
+        : undefined),
 });
