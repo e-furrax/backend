9c70c8ab7706c24e714c20a0f2ebc2f7ed06fed5
Tom Délié <tomdelie78@gmail.com>
2021-06-30 21:00:00 +0200

    chore: fix redis password

--- src/redis.ts
+++ src/redis.ts
@@ -3,7 +3,5 @@ import Redis from 'ioredis';
 export const redis = new Redis({
     host: process.env.REDIS_ENDPOINT || 'redis_container',
     port: 6379,
-    ...(process.env.REDIS_PASSWORD
-        ? { password: process.env.REDIS_PASSWORD }
-        : undefined),
+    password: process.env.REDIS_PASSWORD || 'furrax',
 });
