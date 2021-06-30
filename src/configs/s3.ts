9b69c159dcb15a0db01eba05f0f89d819a8a9749
Thomas Gorszczyk <thomasgo77@gmail.com>
2021-06-30 18:41:04 +0200

    chore: changed env variable

--- src/configs/s3.ts
+++ src/configs/s3.ts
@@ -8,7 +8,7 @@ const S3Config = {
             accessKeyId: process.env.AWS_ACCESS_KEY_ID,
             secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
         },
-        region: process.env.AWS_S3_REGION,
+        region: process.env.AWS_DEFAULT_REGION,
         params: {
             ACL: 'public-read',
             Bucket: process.env.AWS_S3_BUCKET,
