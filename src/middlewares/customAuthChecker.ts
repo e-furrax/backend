b984437d322a3858e21f40235bd35da04c6aac1c
Himyo <himyo97@gmail.com>
2021-07-06 04:19:29 +0200

    fix role check

--- src/middlewares/customAuthChecker.ts
+++ src/middlewares/customAuthChecker.ts
@@ -2,8 +2,14 @@ import { AuthChecker } from 'type-graphql';
 import { MyContextPayload } from '@/types/MyContext';
 import { verify } from 'jsonwebtoken';
 import { UserRole } from '@/entities/postgres/User';
+import { Container } from 'typedi';
+import { Connection } from 'typeorm';
+import { User } from '@/entities/postgres/User';
 
-export const customAuthChecker: AuthChecker<any> = ({ context }, roles) => {
+export const customAuthChecker: AuthChecker<any> = async (
+    { context },
+    roles
+) => {
     const authorization =
         context.req?.headers?.authorization ||
         context.extended?.['authorization'];
@@ -15,7 +21,13 @@ export const customAuthChecker: AuthChecker<any> = ({ context }, roles) => {
     try {
         const token = authorization.split(' ')[1];
         const payload = verify(token, 's3cr3tk3y') as MyContextPayload;
-        userRole = payload.role;
+        const postgres = Container.get('POSTGRES_MANAGER') as Connection;
+        const user = await postgres
+            .getRepository(User)
+            .findOne({ where: { id: payload.userId } })
+            .then((user) => user);
+        userRole = user?.role || '';
+        payload.role = userRole;
         context.payload = payload;
     } catch (err) {
         return false;
